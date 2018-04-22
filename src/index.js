a6113de5cc3a190197c16f44d9a0158aba502547
Matt Hamlin <hamlim@outlook.com>
2018-04-21 22:05:01 -0400

    simple folder names, files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-export RefManager from './RefManager/index.js'
-export Toggle from './Toggle/index.js'
-export FocusManager from './FocusManager/index.js'
+import RefManager from './ref-manager/index'
+import FocusManager from './focus-manager/index'
+import Toggle from './toggle/index'
+
+export { Toggle, RefManager, FocusManager }
