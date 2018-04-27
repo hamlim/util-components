dab4a92510ad1f9016fdd3bacdb2896c2ef211fd
Matt Hamlin <hamlim@outlook.com>
2018-04-26 20:22:23 -0400

    Add a basic logger component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import RefManager from './ref-manager/index'
 import FocusManager from './focus-manager/index'
 import Toggle from './toggle/index'
+import Logger from './logger/index'
 
-export { Toggle, RefManager, FocusManager }
+export { Toggle, RefManager, FocusManager, Logger }
